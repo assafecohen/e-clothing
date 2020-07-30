1a875f17b47929c77b8ace78e45ba66a9d13cff9
assafecohen <assafe.cohen@gmail.com>
2020-07-30 18:45:46 +0300

    updating our app with shopPage collectionpriview collectionitem

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import HomePage from './pages/homepages/homepage';
 
+import HomePage from './pages/homepages/homepage';
+import ShopPage from './pages/shop/shopPage';
 import './App.css';
 
 const HatsPage = () => (
@@ -15,7 +16,7 @@ function App() {
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/hats' component={HatsPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
       {/* <HomePage /> */}
     </div>
---
+++ src/components/preview-collection/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import CollectionPriview from '../collection-item';
+import './preview-collection.scss';
+import CollectionItem from '../collection-item';
+
+const PreviewCollection = ({ title, items }) => (
+  <div className='collection-preview'>
+    <h1 className='title'>{title.toUpperCase()}</h1>
+    <div className='preview'>
+      {items
+        .filter((item, idx) => idx < 4)
+        .map(({ id, ...otherProps }) => (
+          <CollectionItem key={id} {...otherProps} />
+        ))}
+    </div>
+  </div>
+);
+
+export default PreviewCollection;
